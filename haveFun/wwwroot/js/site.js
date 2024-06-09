a909adf9e82fad6bf0354833eb85b0f7ba2e7481
1300Aloce4403 <lskysm@gmail.com>
2024-06-09 14:13:12 +0100

    Add project files.

---
+++ haveFun/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function newFace() {
+    document.getElementById("myText").style.fontFamily = "Lucida Grande";
+}
+
+function oldFace() {
+    document.getElementById("myText").style.fontFamily = "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
+}
+
